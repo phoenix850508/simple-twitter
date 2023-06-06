a83d5451849cbb879d81131f9d57898dc32e4693
phoenix850508 <tom850508@gmail.com>
2023-06-06 12:05:07 +0800

    feat: add scss variables and css reset

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "styles/App.css";
+import "styles/reset.scss";
+import "styles/base.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>$$ % Learn React 12312431asd</p>
+      <h1>h1. Bold / 68px</h1>
+      <h2>123124</h2>
+      <h3>124124</h3>
+      <h4>12312</h4>
     </div>
   );
 }
