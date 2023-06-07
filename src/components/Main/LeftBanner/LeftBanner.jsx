d9a2aaf9fa8d32ac0a81d0ca4d6b76e4d2ef0175
JohnnyFangFangFang <johnny781222@gmail.com>
2023-06-07 12:50:56 +0800

    Refactor leftbanner css

--- src/components/Main/LeftBanner/LeftBanner.jsx
+++ src/components/Main/LeftBanner/LeftBanner.jsx
@@ -5,12 +5,16 @@ import userInfo from 'icons/userInfo.svg'
 import settings from 'icons/settings.svg'
 import logout from 'icons/logout.svg'
 
-
-// 左欄推文按鈕
-function LeftBannerTweet() {
+export default function LeftBanner() {
   return (
-    <div>
-      <button className={styles.LeftBannerTweetBtn}><p className={styles.LeftBannerTweetText}>推文</p></button>
+    <div className={styles.leftBannerContainer}>
+      <div className={styles.leftBannerLogo}>
+        <img src={ac_logo} alt="ac_logo.svg" />
+      </div>
+      <LeftBannerItems />
+      <div className={styles.leftBannerLogout}>
+        <Logout />
+      </div>
     </div>
   )
 }
@@ -33,27 +37,22 @@ function LeftBannerItems() {
   )
 }
 
-// 登出按鈕
-function Logout() {
+// 左欄推文按鈕
+function LeftBannerTweet() {
   return (
-    <div className={styles.leftBannerItem}>
-      <button className={styles.LeftBannerLogoutBtn}>
-        <img className={styles.leftBannerIcon} src={logout} alt="logout.svg" />
-      </button>
+    <div>
+      <button className={styles.leftBannerTweetBtn}><p className={styles.leftBannerTweetText}>推文</p></button>
     </div>
   )
 }
 
-export default function LeftBanner() {
+// 登出按鈕
+function Logout() {
   return (
-    <div className={styles.leftBannerContainer}>
-      <div className={styles.leftBannerLogo}>
-        <img src={ac_logo} alt="ac_logo.svg" />
-      </div>
-      <LeftBannerItems />
-      <div className={styles.leftBannerLogout}>
-        <Logout />
-      </div>
+    <div className={styles.leftBannerItem}>
+      <button className={styles.leftBannerLogoutBtn}>
+        <img className={styles.leftBannerIcon} src={logout} alt="logout.svg" />
+      </button>
     </div>
   )
 }
