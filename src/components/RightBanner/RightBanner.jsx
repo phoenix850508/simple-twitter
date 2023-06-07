ddf62c1f11c4276b0a61bfd80cef1a69ef7cf14b
JohnnyFangFangFang <johnny781222@gmail.com>
2023-06-07 16:21:32 +0800

    Start rightbanner

--- src/components/RightBanner/RightBanner.jsx
+++ src/components/RightBanner/RightBanner.jsx
@@ -1,1 +1,33 @@
-'test'
+import styles from "./RightBanner.module.scss";
+import follow from 'icons/follow.svg'
+import following from 'icons/following.svg'
+
+
+export default function RightBanner() {
+  return (
+    <div className={styles.RightBannerContainer}>
+      <div className={styles.RightBannerTitle}>
+        推薦跟隨
+      </div>
+      <RecommendCollection />
+    </div>
+  )
+}
+
+function RecommendCollection() {
+  return (
+    <div>
+      <RecommendItem />
+      <RecommendItem />
+      <RecommendItem />
+    </div>
+  )
+}
+
+function RecommendItem() {
+  return (
+    <div className={styles.RecommendItemContainer}>
+      RecommendItem
+    </div>
+  )
+}
