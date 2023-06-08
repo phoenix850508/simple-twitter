e9e2ccbbfe26c274890dcc1225ecf7631d2959c1
JohnnyFangFangFang <johnny781222@gmail.com>
2023-06-08 16:51:43 +0800

    feat: finish tweets dummydata and tweetcollection data logic

--- src/components/TweetCollection/TweetCollection.jsx
+++ src/components/TweetCollection/TweetCollection.jsx
@@ -1,12 +1,28 @@
 import styles from "./TweetCollection.module.scss";
+import { useState } from 'react';
 import TweetItem from "components/TweetItem/TweetItem.jsx";
+import dummyTweets from "./dummyTweets";
 
 export default function TweetCollection() {
+  const [tweets, settweets] = useState(dummyTweets);
+
   return (
     <div className={styles.tweetCollectionContainer}>
-      <TweetItem />
-      <TweetItem />
-      <TweetItem />
+      {tweets.map((tweet) => {
+        const { name, account, avatar } = tweet.users
+        const { id, description, createdAt, replyCount, likeCount } = tweet.tweets
+        return (
+          <TweetItem
+            key={id}
+            name={name}
+            account={account}
+            description={description}
+            createdAt={createdAt}
+            replyCount={replyCount}
+            likeCount={likeCount}
+          />
+        );
+      })}
     </div>
   )
 }
