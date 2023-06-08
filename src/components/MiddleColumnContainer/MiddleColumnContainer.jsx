40ca9d68df17f30fde1777782c3eafcff00f7ad4
JohnnyFangFangFang <johnny781222@gmail.com>
2023-06-08 21:26:25 +0800

    feat: scroll function of main middle column

--- src/components/MiddleColumnContainer/MiddleColumnContainer.jsx
+++ src/components/MiddleColumnContainer/MiddleColumnContainer.jsx
@@ -2,8 +2,10 @@ import styles from "./MiddleColumnContainer.module.scss";
 
 export default function MiddleColumnContainer({ children }) {
   return (
-    <div className={styles.middleColumnContainer}>
-      {children}
+    <div className={`${styles.ContainerForScrollbar} ${styles.scrollbar}`}>
+      <div className={styles.middleColumnContainer}>
+        {children}
+      </div>
     </div>
   )
 }
