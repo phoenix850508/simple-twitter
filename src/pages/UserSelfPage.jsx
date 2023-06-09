397a934c4d7f83c9a788287ef3bf75d874fd1523
JohnnyFangFangFang <johnny781222@gmail.com>
2023-06-09 20:38:55 +0800

    start doing change user content

--- src/pages/UserSelfPage.jsx
+++ src/pages/UserSelfPage.jsx
@@ -1,16 +1,26 @@
+import { useState } from 'react'
 import MainContainer from "components/MainContainer/MainContainer.jsx";
 import LeftBanner from "components/LeftBanner/LeftBanner.jsx";
 import RightBanner from "components/RightBanner/RightBanner.jsx";
 import MiddleColumnContainer from "components/MiddleColumnContainer/MiddleColumnContainer.jsx";
 import TopUserSection from "components/TopUserSection/TopUserSection.jsx";
+import ChangeUserContent from "components/ChangeUserContent/ChangeUserContent.jsx";
 // import TweetCollection from "components/TweetCollection/TweetCollection.jsx";
 
 export default function UserSelfPage() {
+  // 購物籃商品清單最新狀態
+  const [userContent, setUserContent] = useState('tweets')
+
+  function handleChangeUserContentClick(targetValue) {
+    setUserContent(targetValue)
+  }
+
   return (
     <MainContainer>
       <LeftBanner />
       <MiddleColumnContainer>
         <TopUserSection />
+        <ChangeUserContent userContent={userContent} handleChangeUserContentClick={handleChangeUserContentClick} />
       </MiddleColumnContainer>
       <RightBanner />
     </MainContainer>
