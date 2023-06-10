c9e066d1f5314c9569423389f3bf903527b6d5a9
JohnnyFangFangFang <johnny781222@gmail.com>
2023-06-10 20:29:34 +0800

    feat: finish basic structure of api-tweets

---
+++ src/api/tweets.js
@@ -0,0 +1,35 @@
+// axios 設定檔
+import axios from "axios";
+const baseUrl = "https://mighty-woodland-74272.herokuapp.com/api";
+
+// 新增一個 instance，使用方法可見 axios interceptor readme
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+});
+
+// 在axiosInstance 使用 interceptors 方法
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error(error);
+  }
+);
+
+// 因為還沒做 Context，這邊可先用 user1 去 login 測試
+// 瀏覽 tweets
+export const getTweets = async () => {
+  try {
+    const res = await axiosInstance.get(`${baseUrl}/tweets`);
+    console.log(res);
+    // 這邊要注意回傳內容，要有兩層 data 才抓得到
+    return res.data.data;
+  } catch (error) {
+    console.error("[Get Tweets failed]: ", error);
+  }
+};
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,17 +1,39 @@
+// React Hook
+import { useState, useEffect } from 'react'
+// 元件類
 import MainContainer from "components/MainContainer/MainContainer.jsx";
 import LeftBanner from "components/LeftBanner/LeftBanner.jsx";
 import RightBanner from "components/RightBanner/RightBanner.jsx";
 import MiddleColumnContainer from "components/MiddleColumnContainer/MiddleColumnContainer.jsx";
 import TopTweetSection from "components/TopTweetSection/TopTweetSection.jsx";
 import TweetCollection from "components/TweetCollection/TweetCollection.jsx";
+// API
+import { getTweets } from '../api/tweets';
+
 
 export default function MainPage() {
+  // tweets 存在這
+  const [tweets, setTweets] = useState([]);
+
+  // 透過 API 撈初始資料
+  useEffect(() => {
+    const getTweetsAsync = async () => {
+      try {
+        const tweets = await getTweets();
+        setTweets(tweets.map((tweet) => ({ ...tweet })));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getTweetsAsync();
+  }, []);
+
   return (
     <MainContainer>
       <LeftBanner />
       <MiddleColumnContainer>
         <TopTweetSection />
-        <TweetCollection />
+        <TweetCollection tweets={tweets} />
       </MiddleColumnContainer>
       <RightBanner />
     </MainContainer>
