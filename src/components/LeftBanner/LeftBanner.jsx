8283111eee2837b011064053909172cfe2c23c2d
phoenix850508 <tom850508@gmail.com>
2023-06-11 20:49:22 +0800

    feat: add navigate pages and logout function

--- src/components/LeftBanner/LeftBanner.jsx
+++ src/components/LeftBanner/LeftBanner.jsx
@@ -7,6 +7,8 @@ import settings from 'icons/settings.svg'
 import TopTweetModal from 'components/TopTweetSection/TopTweetComponents/TopTweetModal'
 import {useState} from 'react'
 import {postTweets} from 'api/tweets.js'
+import {useAuth} from 'context/AuthContext.jsx'
+import { useNavigate } from 'react-router-dom';
 
 export default function LeftBanner() {
   const [show, setShow] = useState(false);
@@ -14,6 +16,21 @@ export default function LeftBanner() {
   const handleShow = () => setShow(true);
   const [tweet, setTweet] = useState('')
   const [isUpdating, setIsUpdating] = useState(false)
+  const {logout} = useAuth()
+  const navigate = useNavigate()
+  const handleLogout = () => {
+    logout();
+    navigate('/login')
+  }
+  const handleHomePageClick = () => {
+    navigate('/main')
+  }
+  const handleUserSelfPageClick = () => {
+    navigate('/user/self')
+  }
+  const handleSettingPageClick = () => {
+    navigate('/setting')
+  }
   //在左邊欄也增加新增推文的API請求
   const handleSubmit = async () => {
     try { 
@@ -41,9 +58,9 @@ export default function LeftBanner() {
       <div className={styles.leftBannerLogo}>
         <img src={ac_logo} alt="ac_logo.svg" />
       </div>
-      <LeftBannerItems onClick={handleShow} />
+      <LeftBannerItems onTweetClick={handleShow} onHomePageClick={handleHomePageClick} onUserSelfClick={handleUserSelfPageClick} onSettingClick={handleSettingPageClick} />
       <div className={styles.leftBannerLogout}>
-        <Logout />
+        <Logout onClick={handleLogout} />
       </div>
       <TopTweetModal show={show} handleClose={handleClose} buttonText={"推文"} onSubmit={handleSubmit} onChange={(tweetInput) => setTweet(tweetInput)} />
     </div>
@@ -51,28 +68,28 @@ export default function LeftBanner() {
 }
 
 // 左欄項目，不含 logo
-function LeftBannerItems({onClick}) {
+function LeftBannerItems({onTweetClick, onHomePageClick, onUserSelfClick, onSettingClick}) {
   return (
     <div>
-      <div className={styles.leftBannerItem}>
+      <div className={styles.leftBannerItem} onClick={onHomePageClick}>
         <img className={styles.leftBannerIcon} src={homeActive} alt="homeActive.svg" />
       </div>
-      <div className={styles.leftBannerItem}>
+      <div className={styles.leftBannerItem} onClick={onUserSelfClick}>
         <img className={styles.leftBannerIcon} src={userInfo} alt="userInfo.svg" />
       </div>
-      <div className={styles.leftBannerItem}>
+      <div className={styles.leftBannerItem} onClick={onSettingClick}>
         <img className={styles.leftBannerIcon} src={settings} alt="settings.svg" />
       </div>
-      <LeftBannerTweet onClick={onClick} />
+      <LeftBannerTweet onClick={onTweetClick} />
     </div>
   )
 }
 
 // 左欄推文按鈕
-function LeftBannerTweet({onClick}) {
+function LeftBannerTweet({onTweetClick}) {
   return (
     <div>
-      <button className={styles.leftBannerTweetBtn} onClick={onClick}><p className={styles.leftBannerTweetText}>推文</p></button>
+      <button className={styles.leftBannerTweetBtn} onClick={onTweetClick}><p className={styles.leftBannerTweetText}>推文</p></button>
     </div>
   )
 }
