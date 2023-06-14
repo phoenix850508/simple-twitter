c3d75e8a903e2cbec6ad24120efa7d01b4acacb9
JohnnyFangFangFang <johnny781222@gmail.com>
2023-06-14 12:00:28 +0800

    fix: github page homepage redirect issue

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,22 @@
-function HomePage() {
-    return (
-    <div>
-      <p>homepage</p>
-    </div>
-)}
+// 引用 React Hook
+import { useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+// 引用 context
+import { AuthContext } from 'context/AuthContext.jsx'
 
-export default HomePage
+const HomePage = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/main');
+    } else {
+      navigate('/login');
+    }
+  }, [navigate, isAuthenticated]);
+
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
