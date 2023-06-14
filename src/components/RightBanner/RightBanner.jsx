30e14d09f0ed97b14335746f9ccba967f24f33bb
JohnnyFangFangFang <johnny781222@gmail.com>
2023-06-15 03:37:39 +0800

    redirect user to user self if clicking his own avatar in a tweet

--- src/components/RightBanner/RightBanner.jsx
+++ src/components/RightBanner/RightBanner.jsx
@@ -11,10 +11,15 @@ import { getTopUsers, postUserFollow, deleteUserFollow } from "api/tweets";
 export default function RightBanner() {
   // 拿 authToken
   const authToken = localStorage.getItem("authToken");
+  // userInfo 資料從 localStorage 拿
+  const savedUserInfoParsed = JSON.parse(localStorage.getItem("userInfo"))
+  const savedUserId = savedUserInfoParsed.id
+
   // tweets 存在這
   const [topUsers, setTopUsers] = useState([]);
   // 本來想設置 flag，讓點擊跟隨時畫面能重新渲染，但無效，故轉採子元件設 state 方式
-  // const [flagForRendering, setFlagForRendering] = useState(false);
+  const [flagForRendering, setFlagForRendering] = useState(false);
+  console.log('flagForRendering', flagForRendering)
 
   // 去撈跟隨 API
   const postUserFollowAsync = async (authToken, id) => {
@@ -37,12 +42,13 @@ export default function RightBanner() {
   }
 
   // 追蹤按鈕邏輯
-  function handleFollowClick(idTopUserReceived, isFollowed) {
+  async function handleFollowClick(idTopUserReceived, isFollowed) {
     if (isFollowed) {
-      deleteUserFollowAsync(authToken, idTopUserReceived)
+      await deleteUserFollowAsync(authToken, idTopUserReceived)
     } else {
-      postUserFollowAsync(authToken, idTopUserReceived)
+      await postUserFollowAsync(authToken, idTopUserReceived)
     }
+    setFlagForRendering(!flagForRendering)
   }
 
   // 透過 API 撈 topUsers 資料
@@ -57,19 +63,19 @@ export default function RightBanner() {
       }
     };
     getTopUsersAsync()
-  }, []);
+  }, [flagForRendering]);
 
   return (
     <div className={styles.rightBannerContainer}>
       <div className={styles.rightBannerTitle}>
         推薦跟隨
       </div>
-      <RecommendCollection topUsers={topUsers} handleFollowClick={handleFollowClick} />
+      <RecommendCollection topUsers={topUsers} handleFollowClick={handleFollowClick} savedUserId={savedUserId} />
     </div>
   )
 }
 
-function RecommendCollection({ topUsers, handleFollowClick }) {
+function RecommendCollection({ topUsers, handleFollowClick, savedUserId }) {
   return (
     <div className={styles.recommendCollectionContainer}>
       {topUsers.map((topUser) => {
@@ -83,6 +89,7 @@ function RecommendCollection({ topUsers, handleFollowClick }) {
             avatar={avatar}
             isFollowed={isFollowed}
             handleFollowClick={handleFollowClick}
+            savedUserId={savedUserId}
           />
         );
       })}
@@ -90,7 +97,7 @@ function RecommendCollection({ topUsers, handleFollowClick }) {
   )
 }
 
-function RecommendItem({ id, name, account, avatar, isFollowed, handleFollowClick }) {
+function RecommendItem({ id, name, account, avatar, isFollowed, handleFollowClick, savedUserId }) {
   // 暫存 isFollowed，讓點擊跟隨時畫面能即時回饋給使用者，同時往後端送資料
   const [isFollowedTemp, setIsFollowedTemp] = useState(isFollowed);
 
@@ -108,8 +115,12 @@ function RecommendItem({ id, name, account, avatar, isFollowed, handleFollowClic
       <button
         className={styles.followBtn}
         onClick={() => {
-          setIsFollowedTemp(!isFollowedTemp)
-          handleFollowClick(id, isFollowed)
+          if (id === savedUserId) {
+            alert('使用者不能跟隨自己哦，請跟隨其他使用者吧～')
+          } else {
+            handleFollowClick(id, isFollowed)
+            setIsFollowedTemp(!isFollowedTemp)
+          }
         }}
       >
         {isFollowedTemp ? <img src={following} alt="following.svg" /> : <img src={follow} alt="follow.svg" />}
