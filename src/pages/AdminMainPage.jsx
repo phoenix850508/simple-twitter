dd223534340cd349ffdcab9bb3dbe94fb389a70d
JohnnyFangFangFang <johnny781222@gmail.com>
2023-06-15 12:40:29 +0800

    feat: admin left banner btn color change

--- src/pages/AdminMainPage.jsx
+++ src/pages/AdminMainPage.jsx
@@ -3,9 +3,12 @@ import AdminTweetsList from "components/AdminTweetsList/AdminTweetsList.jsx";
 import AdminContainer from "components/Admin/AdminContainer"
 
 export default function AdminMainPage() {
+  // 為了顯示左側按鈕顏色需做判斷
+  const currentPage = 0
+
   return (
     <AdminContainer>
-      <LeftBannerAdmin />
+      <LeftBannerAdmin currentPage={currentPage} />
       <AdminTweetsList />
     </AdminContainer>
   )
--- src/pages/AdminUsersPage.jsx
+++ src/pages/AdminUsersPage.jsx
@@ -3,21 +3,23 @@ import UserCard from 'components/Admin/UserCard.jsx'
 import AdminContainer from 'components/Admin/AdminContainer.jsx'
 import AdminRightContainer from 'components/Admin/AdminRightContainer.jsx'
 import dummyUserCards from 'components/Admin/dummyUserCards.js'
-import {useState, useEffect} from 'react'
-import {getAllUsers} from 'api/tweets'
+import { useState, useEffect } from 'react'
+import { getAllUsers } from 'api/tweets'
 
 export default function AdminUsersPage() {
+  // 為了顯示左側按鈕顏色需做判斷
+  const currentPage = 1
   const [users, setUsers] = useState({})
   let userAvatar = ''
   const savedUserInfo = localStorage.getItem("userInfo")
   const savedUserInfoParsed = JSON.parse(savedUserInfo)
-  if(users && users.data) {
-    const {id, avatar} = users.data
+  if (users && users.data) {
+    const { id, avatar } = users.data
     userAvatar = avatar
     console.log(id, avatar)
   }
   useEffect(() => {
-    const getAllUsersAsync = async() => {
+    const getAllUsersAsync = async () => {
       const response = await getAllUsers()
       setUsers(response)
     }
@@ -26,13 +28,13 @@ export default function AdminUsersPage() {
   return (
     <div>
       <AdminContainer>
-       <LeftBannerAdmin />
-       <AdminRightContainer title={"使用者列表"}>
-        {userAvatar}
-        {/* {users.map((user) => {
+        <LeftBannerAdmin currentPage={currentPage} />
+        <AdminRightContainer title={"使用者列表"}>
+          {userAvatar}
+          {/* {users.map((user) => {
           return <UserCard key={user.id} avatar={user.avatar} background={user.banner} name={user.name} account={user.account} tweetCount={user.tweetCount} likeCount={user.likeCount} followersCount={user.followersCount} followingCount={user.followingCount} />
         })} */}
-       </AdminRightContainer>
+        </AdminRightContainer>
       </AdminContainer>
     </div>
   )
