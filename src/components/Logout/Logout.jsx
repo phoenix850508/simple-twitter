29f1229d600439a93a9dd522f7dc3b20c7d32c94
JohnnyFangFangFang <johnny781222@gmail.com>
2023-06-17 19:26:31 +0800

    feat: logout btn hover

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,12 +1,13 @@
 import styles from "./Logout.module.scss";
 import logout from 'icons/logout.svg'
+// import logoutActive from 'icons/logoutActive.svg'
 
 // 登出按鈕
-export default function Logout({onClick}) {
+export default function Logout({ onClick }) {
   return (
     <div className={styles.leftBannerItem}>
       <button className={styles.leftBannerLogoutBtn} onClick={onClick}>
-        <img className={styles.leftBannerIcon} src={logout} alt="logout.svg" />
+        <img className={`${styles.leftBannerIcon} ${styles.iconLogout}`} src={logout} alt="logout.svg" />
       </button>
     </div>
   )
