0ecceb7701d9f7b75e5ae4871f296538143b496f
phoenix850508 <tom850508@gmail.com>
2023-06-17 17:00:26 +0800

    fix: UserSelfPage TopUserSection should not save the data from edit modal input

--- src/components/TopUserSection/TopUserSection.jsx
+++ src/components/TopUserSection/TopUserSection.jsx
@@ -4,7 +4,7 @@ import dummyBackgroundImage from 'icons/dummyBackgroundImage.svg'
 // import dummyUserPhoto from 'icons/dummyUserPhoto.svg'
 import editUserInfoBtn from 'icons/editUserInfoBtn.svg'
 import PrePageBtn from 'components/PrevPageBtn/PrevPageBtn.jsx'
-import { useState, useContext, useEffect, useRef } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import clsx from 'clsx'
 import Modal from 'react-bootstrap/Modal';
 import cross from 'icons/cross.svg'
@@ -31,7 +31,7 @@ export default function TopUserSection({ handleFollowDetailClick, followingCount
   const savedUserInfo = JSON.parse(localStorage.getItem("userInfo"))
   const savedUserInfoId = savedUserInfo.id
   // 其他沒從 API 撈的直接用 localStorage 拿，code 才不用改太多
-  const { account } = savedUserInfo;
+  // const { account } = savedUserInfo;
 
   // following 人數暫存在這，而 follower 人數不會因 user 行為而改變所以不用放
   const [followingCountTemp, setFollowingCountTemp] = useState(followingCount)
@@ -96,13 +96,12 @@ export default function TopUserSection({ handleFollowDetailClick, followingCount
     }
     getUserAsync()
     setIsUserEdited(false)
-    console.log("我被執行")
   }, [getUser, savedUserInfoId, setIsUserEdited, followingCount])
 
 
   return (
     <div>
-      <PrePageBtn toPage='/main' name={name} tweetCount={tweetCount} />
+      <PrePageBtn toPage='/main' name={tempDataObject && tempDataObject.name} tweetCount={tweetCount} />
       <div className={styles.topUserInfoWrapper}>
         <img className={styles.topUserBanner} src={tempDataObject ? (tempDataObject.banner ? tempDataObject.banner : dummyBackgroundImage) : dummyBackgroundImage} alt="dummyBackgroundImage.svg" />
         <img className={styles.topUserPhoto} src={tempDataObject ? (tempDataObject.avatar ? tempDataObject.avatar : avatarDefaultMini) : (savedUserInfo.avatar ? savedUserInfo.avatar : avatarDefaultMini)} alt='avatar' />
@@ -110,9 +109,9 @@ export default function TopUserSection({ handleFollowDetailClick, followingCount
           <img src={editUserInfoBtn} alt="editUserInfoBtn.svg" />
         </button>
         <div className={styles.topUserWordsWrapper}>
-          <div className={styles.topUserName}>{tempDataObject ? tempDataObject.name : name}</div>
-          <div className={styles.topUserAccount}>@{tempDataObject ? tempDataObject.account : account}</div>
-          <div className={styles.topUserIntro}>{intro}</div>
+          <div className={styles.topUserName}>{tempDataObject && tempDataObject.name}</div>
+          <div className={styles.topUserAccount}>@{tempDataObject && tempDataObject.account}</div>
+          <div className={styles.topUserIntro}>{tempDataObject && tempDataObject.introduction}</div>
           <div className={styles.topUserFollowWrapper}>
             <div>
               <button
@@ -130,7 +129,7 @@ export default function TopUserSection({ handleFollowDetailClick, followingCount
                 value='followers'
                 onClick={e => { handleFollowDetailClick(e.currentTarget.value) }}
               >
-                <span className={styles.topUserFollowCount}>{tempDataObject ? tempDataObject.followerCount : followerCount}</span><span className={styles.topUserFollowWord}>跟隨者</span>
+                <span className={styles.topUserFollowCount}>{followerCount && followerCount}</span><span className={styles.topUserFollowWord}>跟隨者</span>
               </button>
             </div>
           </div>
